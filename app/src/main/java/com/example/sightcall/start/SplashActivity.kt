11b4c44301a0b4f386e82686416473f10f1d70a2
Rafik <rafik.fares@shopmium.com>
2022-07-22 18:22:11 +0200

    init splash screen

---
+++ app/src/main/java/com/example/sightcall/start/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.sightcall.start
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import com.example.sightcall.main.MainActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        splashScreen.setKeepOnScreenCondition { true }
+        lifecycleScope.launch {
+            delay(400)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
