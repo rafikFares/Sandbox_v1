a3abfbc4bd9c95f1af2df07155f5ab5eacf5d47c
Rafik <rafik.fares@shopmium.com>
2022-07-26 18:05:54 +0200

    create entities

---
+++ app/src/main/java/com/example/sightcall/core/repository/local/entity/ItemEntity.kt
@@ -0,0 +1,28 @@
+package com.example.sightcall.core.repository.local.entity
+
+import com.example.sightcall.core.repository.data.GitHubItem
+import io.realm.kotlin.types.RealmObject
+
+class ItemEntity : RealmObject {
+    var name: String = ""
+    var owner: String = ""
+    var language: String = ""
+    var avatarUrl: String = ""
+    var stars: Int = 0
+}
+
+fun GitHubItem.toItemEntity(): ItemEntity = ItemEntity().apply {
+    name = repositoryName
+    owner = repositoryOwner
+    language = repositoryLanguage
+    avatarUrl = repositoryOwnerAvatarUrl
+    stars = repositoryStars
+}
+
+fun ItemEntity.toGitHubItem(): GitHubItem = GitHubItem(
+    repositoryName = name,
+    repositoryOwner = owner,
+    repositoryOwnerAvatarUrl = language,
+    repositoryLanguage = avatarUrl,
+    repositoryStars = stars
+)
