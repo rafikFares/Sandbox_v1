3cba84aa90466a1e773ca054ecb3f0c9a9dbd25c
Rafik <rafik.fares@shopmium.com>
2022-07-26 20:26:31 +0200

    update item card spacing

--- app/src/main/java/com/example/sandbox/main/home/binding/Bindings.kt
+++ app/src/main/java/com/example/sandbox/main/home/binding/Bindings.kt
@@ -3,10 +3,17 @@ package com.example.sandbox.main.home.binding
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sandbox.main.home.adapter.HomeAdapter
+import com.example.uibox.tools.SpaceItemDecoration
 
 @BindingAdapter("bind:initAdapter")
 fun RecyclerView.initAdapter(homeAdapter: HomeAdapter) {
     hasFixedSize()
+    addItemDecoration(SpaceItemDecoration(
+        topDP = 6,
+        leftDP = 12,
+        rightDP = 12,
+        bottomDP = 6,
+    ))
     adapter = homeAdapter
     homeAdapter.notifyDataSetChanged()
 }
