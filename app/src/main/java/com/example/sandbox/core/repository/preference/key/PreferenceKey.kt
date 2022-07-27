3cea331cb5be91c18e263c79fd7df4babb7308c4
Rafik <rafik.fares@shopmium.com>
2022-07-27 12:15:26 +0200

    setup dataStore preferences repository

---
+++ app/src/main/java/com/example/sandbox/core/repository/preference/key/PreferenceKey.kt
@@ -0,0 +1,12 @@
+package com.example.sandbox.core.repository.preference.key
+
+import kotlin.reflect.KClass
+
+enum class PreferenceKey(val keyName: String, val type: KClass<*>) {
+    UserName("user_name", String::class),
+    UserEmail("user_email", String::class),
+    UserToken("user_token", String::class),
+    UserAge("user_age", Int::class);
+
+    fun isCorrectType(other: Any) = other::class == type
+}
