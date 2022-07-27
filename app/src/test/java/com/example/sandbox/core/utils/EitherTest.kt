7678172eb9bc745dd88ed69cc33ad4f31aaa8bb5
Rafik <rafik.fares@shopmium.com>
2022-07-27 12:01:52 +0200

    test either extensions

--- app/src/test/java/com/example/sandbox/core/utils/EitherTest.kt
+++ app/src/test/java/com/example/sandbox/core/utils/EitherTest.kt
@@ -2,10 +2,10 @@ package com.example.sandbox.core.utils
 
 import com.example.sandbox.BaseUnitTest
 import com.example.sandbox.core.exception.SandboxException
+import kotlin.test.Test
 import org.amshove.kluent.shouldBe
 import org.amshove.kluent.shouldBeEqualTo
 import org.amshove.kluent.shouldBeInstanceOf
-import kotlin.test.Test
 
 class EitherTest : BaseUnitTest() {
 
@@ -39,7 +39,6 @@ class EitherTest : BaseUnitTest() {
         )
     }
 
-
     @Test
     fun foldRunSuccessTask() {
         val eitherSuccess = Either.Success("tmp")
@@ -53,5 +52,65 @@ class EitherTest : BaseUnitTest() {
         )
 
         result shouldBeEqualTo "tmp"
+    }
+
+    @Suppress("UNREACHABLE_CODE")
+    @Test
+    fun extensionIfIsSuccessThanCorrect() {
+        val eitherFailure = Either.Failure(SandboxException.EmptyParamsException)
+        val eitherSuccess = Either.Success("tmp")
+        var resultSuccess: String? = null
+        var resultFailure: SandboxException? = null
+
+        eitherFailure.ifIsSuccessThan {
+            resultFailure = it
+        }
+
+        eitherSuccess.ifIsSuccessThan {
+            resultSuccess = it
+        }
+
+        resultFailure shouldBe null
+        resultSuccess shouldBeEqualTo "tmp"
+    }
+
+    @Suppress("UNREACHABLE_CODE")
+    @Test
+    fun extensionIfIsFailureThanCorrect() {
+        val failureException = SandboxException.EmptyParamsException
+        val eitherFailure = Either.Failure(failureException)
+        val eitherSuccess = Either.Success("tmp")
+        var resultSuccess: String? = null
+        var resultFailure: SandboxException? = null
+
+        eitherFailure.ifIsFailureThan {
+            resultFailure = it
+        }
+
+        eitherSuccess.ifIsFailureThan {
+            resultSuccess = it
+        }
+
+        resultFailure shouldBe failureException
+        resultSuccess shouldBe null
+    }
+
+    @Suppress("UNREACHABLE_CODE")
+    @Test
+    fun extensionIfIsFailureWithThanCorrect() {
+        val failureException = SandboxException.EmptyParamsException
+        val eitherFailure = Either.Failure(failureException)
+        val eitherSuccess = Either.Success("tmp")
+        var resultFailure: SandboxException? = null
+
+        eitherFailure.ifIsFailureWithThan(failureException) {
+            resultFailure = it
+        }
+
+        eitherSuccess.ifIsFailureWithThan(failureException) {
+            fail("unreachable code got reached")
+        }
+
+        resultFailure shouldBe failureException
     }
 }
